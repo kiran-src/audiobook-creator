4c0308c837335d125f96e70185c0b0b42161cef1
Kiran <kiransettyks@gmail.com>
2022-04-08 16:20:33 +0200

    Create a voice file using AWS Polly and a sample text

--- main.py
+++ main.py
@@ -4,5 +4,20 @@ import boto3
 ACCESS_KEY = os.environ.get("ACCESS_KEY")
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
-# poly = boto3.client('polly')
-print(boto3.client('polly'))
+poly = boto3.client('polly', region_name='us-west-2',aws_access_key_id=ACCESS_KEY, aws_secret_access_key=SECRET_KEY)
+# print(client.describe_voices(LanguageCode='en-GB', Engine='neural', IncludeAdditionalLanguageCodes=False, NextToken='string'))
+
+def play_sound(text):
+    response = poly.synthesize_speech(Engine='neural',
+                                      LanguageCode='en-US',
+                                      OutputFormat='mp3',
+                                      Text=text,
+                                      VoiceId='Joanna',
+                                      TextType='text')
+    body = response['AudioStream'].read()
+
+    with open('voice.mp3', 'wb') as file:
+        file.write(body)
+        file.close()
+
+play_sound("Hello")
